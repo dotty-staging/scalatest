d6eeedbfc1e04f2eff55506f07f93f448cc21407
Hamza REMMAL <hamza@remmal.dev>
2024-05-22 10:31:15 +0200

    Adapt project to Scala 3.5

--- project/GenFactories.scala
+++ project/GenFactories.scala
@@ -137,7 +137,7 @@ $if (arityIsOne)$
    * result should equal (1) (decided by defaultEquality)
    * </pre>
    */
-  def apply[T <: SC](explicit: TC1[T]): Matcher[T] = matcher[T](explicit)
+  def apply[T <: SC](explicit: TC1[T]): Matcher[T] = matcher[T](using explicit)
 
 $endif$
 
